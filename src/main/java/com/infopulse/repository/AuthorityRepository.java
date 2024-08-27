10cc449f80a9c05a6d4950ad074b16f61e270cce
Abraham Melquisedeque Pereira Licá <103894033+AbrahamLica@users.noreply.github.com>
2024-08-27 08:48:33 -0300

    Initial version of InfoPulse generated by generator-jhipster@8.6.0

---
+++ src/main/java/com/infopulse/repository/AuthorityRepository.java
@@ -0,0 +1,12 @@
+package com.infopulse.repository;
+
+import com.infopulse.domain.Authority;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Spring Data JPA repository for the Authority entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ src/main/java/com/infopulse/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Rest layer visual models.
+ */
+package com.infopulse.web.rest.vm;
